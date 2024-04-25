4f02ec37a972164b1f49098196727fd85238f051
Shiyu Fan <149340606+ShiyuFan0820@users.noreply.github.com>
2024-04-25 12:05:03 +0100

    Update data_manager.py

--- src/data_manager.py
+++ src/data_manager.py
@@ -7,31 +7,57 @@ class DataManager:
     ## Define a method to get the city names from the Google Sheet.
     @classmethod
     def GetCityInfo(cls):
-        '''
+        """
 
         :return: City information on the Google Sheet.
-        '''
+        """
         response = requests.get(sheety_endpoint)
         city_info = response.json()['prices']
         return city_info
 
     @classmethod
-    def UpdateCityCode(cls, cities_info):
-        '''
+    def UpdateCityCode(cls, row_id, city_code):
+        """
 
-        :param cities_info: A list of dictionary, each dictionary inludes the updated information of the cities.
+        :param row_id: City's row id.
+        :param city_code: City's code
         :return: None
-        '''
-        for row in cities_info:
+        """
+        new_data = {
+            "price": {
+                "iataCode": city_code
+            }
+        }
+        response = requests.put(
+            url=f"{sheety_endpoint}/{row_id}",
+            json=new_data
+        )
+        print("IATA Code has been updated successfully.")
+        return
+
+    @classmethod
+    def UpdateCheaptestFlight(cls, update_data):
+        """
+
+        :param update_data: A list of dictionary, each dictionary inludes the updated information of the cities.
+        :return: None
+        """
+        for row in update_data:
             new_data = {
                 "price": {
-                    "iataCode": row["iataCode"]
+                    "lowestPrice": row["cheapest_flight"]["price"],
+                    "fromAirport": row["cheapest_flight"]["from_airport"],
+                    "toAirport": row["cheapest_flight"]["to_airport"],
+                    "departureDate": row["cheapest_flight"]["departure_date"],
+                    "departureTime": row["cheapest_flight"]["departure_time"],
+                    "arrivalDate": row["cheapest_flight"]["arrival_date"],
+                    "arrivalTime": row["cheapest_flight"]["arrival_time"]
                 }
             }
             response = requests.put(
                 url=f"{sheety_endpoint}/{row['id']}",
                 json=new_data
             )
-            print(f"{row["city"]}'s IATA Code has been updated successfully.")
+            print(f"{row["city"]}'s flight information has been updated successfully.")
         return
 
