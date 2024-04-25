1412b5db5852f43280fd066662431422ff429c35
Shiyu Fan <149340606+ShiyuFan0820@users.noreply.github.com>
2024-04-25 12:05:43 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,48 +5,65 @@ from datetime import datetime, timedelta
 
 # Use sheety API to get city data in Google Sheet.
 cities_info = DataManager.GetCityInfo()
-print(cities_info)
 
 # Use SearchCityCode of FlightSearch to search for code of city if it's empty.
 for row in cities_info:
     try:
         row['iataCode']
     except KeyError:
+        # If code is empty, find the code and update in the Google Sheet.
         city_name = row['city']
         city_code = FlightSearch.SearchCityCode(city_name)
+        city_id = row["id"]
+        DataManager.UpdateCityCode(city_id, city_code)
         row['iataCode'] = city_code
-
-# Use sheety API to update the city code on the Google Sheet.
-DataManager.UpdateCityCode(cities_info)
 
 # Search for the flight prices from one city to all the destinations in the Google Sheet and collect the result.
 from_city = "London"
 from_city_code = city_code = FlightSearch.SearchCityCode(from_city)
 tomorrow = datetime.now() + timedelta(days=1)
 date_from = tomorrow.strftime("%d/%m/%Y")
-six_months_from_tomorrow = datetime.now() + timedelta(days=(6 * 30))
+six_months_from_tomorrow = datetime.now() + timedelta(days=(4 * 30))
 date_to = six_months_from_tomorrow.strftime("%d/%m/%Y")
-for city in cities_info:
-    to_city_code = city["iataCode"]
+update_data = []
+for city_info in cities_info:
+    to_city_code = city_info["iataCode"]
     flights_data = FlightSearch.SearchFlight(from_city_code, to_city_code, date_from, date_to)
-    for flight in flights_data:
-        from_airport = flight["flyFrom"]
-        to_airport = flight["flyTo"]
-        dep_time = flight["local_departure"].split("T")[1].split(".")[0] # Only extract the time.
-        arr_time = flight["local_arrival"].split("T")[1].split(".")[0] # Only extract the time.
-        price = flight["price"]
-        try:
-            city["flight"]
-        except KeyError:
-            city["flight"] = []
-        finally:
-            city["flight"].append(
-                {
-                    "fromAirport": from_airport,
-                    "toAirport": to_airport,
-                    "departureTime": dep_time,
-                    "arrivalTime": arr_time,
-                    "price": price
-                }
-            )
+    if flights_data:
+        for flight in flights_data:
+            from_airport = flight["flyFrom"]
+            to_airport = flight["flyTo"]
+            dep_date = flight["local_departure"].split("T")[0]
+            dep_time = flight["local_departure"].split("T")[1].split(".")[0] # Only extract the time.
+            arr_date = flight["local_arrival"].split("T")[0]
+            arr_time = flight["local_arrival"].split("T")[1].split(".")[0] # Only extract the time.
+            price = flight["price"]
+            try:
+                city_info["flight"]
+            except KeyError:
+                city_info["flight"] = []
+            finally:
+                city_info["flight"].append(
+                    {
+                        "from_airport": from_airport,
+                        "to_airport": to_airport,
+                        "departure_date": dep_date,
+                        "departure_time": dep_time,
+                        "arrival_date": arr_date,
+                        "arrival_time": arr_time,
+                        "price": price
+                    }
+                )
+        # After finding all flights, then find the cheapest flight.
+        cheapest_flight = min(city_info["flight"], key=lambda x: x["price"])
+        update_data.append(
+            {
+                "city": city_info["city"],
+                "id": city_info["id"],
+                "cheapest_flight": cheapest_flight
+            }
+        )
+# Update the cheapest flight information to Google sheet
+DataManager.UpdateCheaptestFlight(update_data)
+
 
